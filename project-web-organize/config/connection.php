9230fca27826433c66e3940d280cef38a0dc7c87
akunpclab4 <akunpc.rh12345@gmail.com>
2026-09-17 14:21:52 +0700

    Add files via upload

---
+++ project-web-organize/config/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$namaVariable = "Halo semua";
+$namaVariabledua = "king emyu";
+echo "<h1>" . $namaVariable . "<h1/>";
+echo "<h1 style='color:red'>" . $namaVariabledua . "<h1/>";
+
+$namaSaya = "Rifkingg";
+
+
+?>
---
+++ project-web-organize/config/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('connection.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hey dearr!!</h1>
+    
+    <script src="assets/js/hello.js"></script>
+    <?php echo "<p>". $namaSaya ."<p/>" ?>
+
+    <img src="img/emyu.png" alt="Manchaster United" width="250">
+</body>
+</html>
